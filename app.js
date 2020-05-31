4fad762cd542cb46eea073bb92909e72b1bd6a22
Mariel-local <marielk.mya@gmail.com>
2020-05-30 21:07:06 -0400

    agregando chevron animado y algunas correcciones en las cards

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $('.menu-hamb').click(function(){
   $(this).toggleClass('open');
 })
+
+jQuery(document).ready( function() {
+  jQuery('.chevron').on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+  });
+ });
